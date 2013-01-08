6555d771246cf53416f293db2b13994a61e78f36
flycoding <flycoding@yeah.net>
2013-01-08 21:59:57 +0800

    first commit.

---
+++ src/com/flyingh/phone/MainActivity.java
@@ -0,0 +1,34 @@
+package com.flyingh.phone;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void dial(View view) {
+		EditText edtiText = (EditText) findViewById(R.id.telphone);
+		Intent intent = new Intent(Intent.ACTION_DIAL);
+		intent.setData(Uri.parse("tel:" + edtiText.getText().toString()));
+		intent.addCategory(Intent.CATEGORY_DEFAULT);
+		startActivity(intent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
